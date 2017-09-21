bbce97ffe7e45dd369fa171ed59538a1f9db3227
cmg5831 <cmg5831@WGATEE206-82.win.psu.edu>
2017-09-21 12:22:06 -0400

    Created menu skeleton

---
+++ IST361DefenseGame/src/view/MenuUI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import controller.NavigationController;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author cmg5831
+ */
+public class MenuUI extends JFrame{
+    private NavigationController navCtrl;
+    
+    public MenuUI (NavigationController navCtrl) {
+        this.navCtrl = navCtrl;
+        this.setVisible(true);
+        this.setBounds(0, 0, 750, 550);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    }
+}
