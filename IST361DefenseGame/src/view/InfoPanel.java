245f9c0dfd7993475ac72e54dd89184ad7101fac
Jake <Jake@192.168.1.9>
2017-09-25 17:51:31 -0400

    InfoPanel update

--- IST361DefenseGame/src/view/InfoPanel.java
+++ IST361DefenseGame/src/view/InfoPanel.java
@@ -5,6 +5,7 @@
  */
 package view;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -13,4 +14,38 @@ import javax.swing.JPanel;
  */
 public class InfoPanel extends JPanel{
     
+    private JLabel nameLabel;
+    private JLabel scoreLabel;
+    private JLabel moneyLabel;
+    private JLabel healthLabel;
+    private JLabel difficultyLabel;
+    private JLabel tower1Label;
+    private JLabel tower2Label;
+    private JLabel tower3Label;
+    
+    public InfoPanel(){
+        initComponents();
+    }
+    
+    private void initComponents(){
+        nameLabel.setText("Name: ");
+        scoreLabel.setText("Score: ");
+        moneyLabel.setText("Money: ");
+        healthLabel.setText("Health: ");
+        difficultyLabel.setText("Difficulty: ");
+        tower1Label.setText("Homework Tosser: ");
+        tower2Label.setText("Eraser Cannon: ");
+        tower3Label.setText("Paper Football Launcher: ");
+        
+        this.add(nameLabel);
+        this.add(scoreLabel);
+        this.add(moneyLabel);
+        this.add(healthLabel);
+        this.add(difficultyLabel);
+        this.add(tower1Label);
+        this.add(tower2Label);
+        this.add(tower3Label);
+
+       
+    }
 }
