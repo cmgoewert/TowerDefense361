b2f4e3107b5048729c04087ee383c6781f2a7f39
lap5508 <lap5508@Pollock201-145.win.psu.edu>
2017-09-26 16:17:49 -0400

    Merge origin/master
    Conflicts:
    	IST361Round1/src/ist361round1/NavigationController.java
    	IST361Round1/src/ist361round1/NavigationView.java

--- IST361DefenseGame/src/view/GameBoardPanel.java
+++ IST361DefenseGame/src/view/GameBoardPanel.java
@@ -9,8 +9,10 @@ import controller.GameController;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 
 /**
  *
@@ -20,6 +22,12 @@ public class GameBoardPanel extends JPanel{
     ArrayList<GameTilePanel> theTiles = new ArrayList<GameTilePanel>();
     GameController parentCtrl;
     GridLayout theLayout;
+    JPanel gamePanel;
+    boolean isPresent = false;
+    int countThis = 0;
+    
+    int pathTiles[]={3,4,15,16,27,28,29,30,31,32,33,39,40,41,42,43,44,45,56,57,68,69,80,81,92,93,96,97,98,99,100,101,102,103,104,105,108,109,110,111,112,113,114,115,116,117};
+    int size = pathTiles.length;
     
     public GameBoardPanel(GameController theCtrl){
         parentCtrl = theCtrl;
@@ -31,19 +39,28 @@ public class GameBoardPanel extends JPanel{
         this.setLayout(theLayout);
    
         for (int i = 0; i < theLayout.getColumns() * theLayout.getRows(); i++) {
-            if (i % 12 == 3 || i % 12 == 4) {
-                GameTilePanel theLabel = new GameTilePanel(parentCtrl, true);
-                theTiles.add(theLabel);
-                this.add(theLabel);
-            } else {
-                GameTilePanel theLabel = new GameTilePanel(parentCtrl, false);
-                theTiles.add(theLabel);
-                this.add(theLabel);
+            isPresent = false;
+                for(int j = 0; j < pathTiles.length; j++){
+                
+                    if(i == pathTiles[j])
+                    {
+                        isPresent = true;
+                    }
+                    
+                }
+                    if (isPresent) {
+                        GameTilePanel theLabel = new GameTilePanel(parentCtrl, true);
+                        theTiles.add(theLabel);
+                        this.add(theLabel);
+                    } else {
+                        GameTilePanel theLabel = new GameTilePanel(parentCtrl, false);
+                        theTiles.add(theLabel);
+                        this.add(theLabel);
+                    }
+                
             }
-
-
-        }
-    }
+        }        
+    
     
     //        for(int i = 0; i < 144; i++){
 //            JPanel thePanel = new JPanel();
