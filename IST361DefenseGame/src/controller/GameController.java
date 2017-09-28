a865a68f3229ddf69eed42ee2c3c1a00e2c40ebb
[Rees] <[mcdevittrees@gmail.com]>
2017-09-28 11:58:45 -0400

    Stubs for game controller

--- IST361DefenseGame/src/controller/GameController.java
+++ IST361DefenseGame/src/controller/GameController.java
@@ -5,6 +5,10 @@
  */
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import view.GameUI;
 
 /**
@@ -16,5 +20,75 @@ public class GameController {
     
     public GameController(){
         theGameUI = new GameUI(this);
+    }
+    
+    //TODO: Logic for when it increases when but is killed, decreses when bug is
+    // leaves the screen
+    class ScoreListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    
+    }
+    
+    //TODO: Logic for when it decreses when bug is leaves the screen
+    class HealthListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    
+    }
+    
+    //TODO: Logic for when it increases when but is killed, decreses when
+    // tower is placed
+    class MoneyListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    
+    }
+    //TODO: Logic for when certain tower is selected
+    class TowerListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    
+    }
+    //TODO: Logic for when tower is placed
+    class PlacementListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    
     }
 }
