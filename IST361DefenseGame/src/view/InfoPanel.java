d0ed61ad35edbafa47de10abaaee524b40631925
eas57 <eas57@104.39.136.215>
2017-10-03 13:05:35 -0400

    Revert "wide next wave button"
    This reverts commit c26476518a215b541dadb948c356ff445bd623a8.

--- IST361DefenseGame/src/view/InfoPanel.java
+++ IST361DefenseGame/src/view/InfoPanel.java
@@ -93,13 +93,8 @@ public class InfoPanel extends JPanel{
         c.gridx = 1;
         c.gridy = 6;
         this.add(getTower3Button(), c);
-        c.gridx = 0;
+        c.gridx = 1;
         c.gridy = 7;
-        this.add(getEmptyLabel(),c);
-        c.gridx = 0;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 2;   
-        c.gridy = 8;
         this.add(getStartButton(), c);
         
     }
